0db3c38a7893f848ca475a092a1fbf88c9a57239
Georgy Vashchenko <gvashchenko@lineate.com>
2020-02-04 00:52:57 +0600

    Enhance Order Line DTO with Beer data from Beer Service. Close #2

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceRestTemplateImpl.java
@@ -0,0 +1,41 @@
+package guru.sfg.beer.order.service.services.beer;
+
+import guru.sfg.beer.order.service.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@Service
+public class BeerServiceRestTemplateImpl implements BeerService {
+
+    private final String BEER_BY_ID_PATH_V1 = "/api/v1/beer/";
+    private final String BEER_BY_UPC_PATH_V1 = "/api/v1/beerUpc/";
+    private final RestTemplate restTemplate;
+
+    private String beerServiceHost;
+
+    public BeerServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public void setBeerServiceHost(String beerServiceHost) {
+        this.beerServiceHost = beerServiceHost;
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerById(UUID beerId) {
+        return Optional.ofNullable(restTemplate.getForObject(beerServiceHost + BEER_BY_ID_PATH_V1 + beerId.toString(), BeerDto.class));
+    }
+
+    @Override
+    public Optional<BeerDto> getBeerByUpc(String upc) {
+        return Optional.ofNullable(restTemplate.getForObject(beerServiceHost + BEER_BY_UPC_PATH_V1 + upc, BeerDto.class));
+    }
+}
